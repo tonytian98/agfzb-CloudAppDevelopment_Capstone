743f7d0e10ad65939b756875f4792a58df6bcd7d
tonytian98 <imtonytian@gmail.com>
2023-08-29 11:50:00 +0800

    week1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,18 +8,17 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-
+    path(route='', view=views.get_dealerships, name='index'),
     # path for about view
-
+    path(route='about/', view=views.about, name='about'),
     # path for contact us view
-
+    path(route='contact/', view=views.contact, name='contact'),
     # path for registration
 
     # path for login
 
     # path for logout
 
-    path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,7 +15,8 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-
+def about(request):
+    return render(request, 'djangoapp/about.html')
 
 # Create an `about` view to render a static about page
 # def about(request):
@@ -24,7 +25,8 @@ logger = logging.getLogger(__name__)
 
 # Create a `contact` view to return a static contact page
 #def contact(request):
-
+def contact(request):
+    return render(request, 'djangoapp/contact.html')
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
 # ...
